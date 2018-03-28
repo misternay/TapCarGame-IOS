7891b606598c0019956bd88b605db5925d9425a3
MisterNay <misternay@odds.team>
2018-03-28 11:39:52 +0700

    Initial Commit

---
+++ OddeRacing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OddeRacing
+//
+//  Created by misternay on 28/3/2561 BE.
+//  Copyright © 2561 misternay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
